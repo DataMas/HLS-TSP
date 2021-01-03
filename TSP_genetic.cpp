3dc413a52e42a4357c18268483b4630a550a8240
Yannis Maslaris <45293009+DataMas@users.noreply.github.com>
2021-01-04 00:33:12 +0200

    First stage of the algorithm, next stage check

--- TSP_genetic.cpp
+++ TSP_genetic.cpp
@@ -9,67 +9,187 @@ const int populationSize = 100;
 const int numberOfNodes = 100;
 const int length = 7;
 
-struct individual {
-	ac_int<length, false> gnome[numberOfNodes];
-	ac_int<length, false> fittness;
-};
+/*###################################################
+Some usefull links
+https://www.obitko.com/tutorials/genetic-algorithms/ga-basic-description.php
+https://2ality.com/2013/03/permutations.html
+https://www.codeproject.com/articles/16286/ai-simple-genetic-algorithm-ga-to-solve-a-card-pro
+###################################################*
 
-
-void randomNum(int start, int finish, ac_int<length,false> &gene) {
+// **Might be useless in the end**
+/*
+void randomNum(int start, int finish, ac_int<length, false>& gene) {
 	// This function returns a random number between "start" and "finish"
 	// It is used for chromosome initialization with random genes
 
 	gene = start + rand() % (start - finish);
 }
 
-bool geneRepeat(individual<length> chromosome, ac_int<length, false> gene) {
+bool geneRepeat(ac_int<length, false> gene, individual<length> chromosome) {
 	// This function checks if a given gene is allready in a given chromosome
 	// We want every gene to occur once in every chromosome
 
-	for (int i = 0; i < chromosome.gnome.size(); i++) {
+	for (int i = 0; i < numberOfNodes; i++) {
 
-		if (chromosome[i] == gene) {
+		if (chromosome.gnome[i] == gene) {
 
 			return true;
 		}
 		return false;
 	}
-}
+}*/
 
-void populationInit(ac_int<length,false> population[populationSize][numberOfNodes], int numberOfNodes, int populationSize) {
-	// This function initializes the chromosomes of the population at arandom
+// ----- INITIALIZE -----
+void populationInit(ac_int<11, false> (&distances) [populationSize][numberOfNodes]) {
+	// This function
+	// input population array, distances array
 
 	for (int i = 0; i < populationSize; i++) {
 
 		for (int j = 0; j < numberOfNodes; j++) {
 
-			do {
+			population[i][j] = j;
+		}
 
-				randomNum(0, numberOfNodes, population[i][j]);
-			}while ( geneRepeat(population[i], population[i][j]) );
+		// Fisher-Yates' shuffling
+		for (int l = 0; l < (numberOfNodes - 1); l++) {
+		
+			short k = min(k + 5 + 1, numberOfNodes);
+			ac_int<length, false> temp = population[i][l];
+			population[i][l] = population[i][k];
 		}
 	}
 }
 
+// ----- To sort based on column in descending order -----
+void sortByColumn(ac_int<11, false>(&vector)[populationSize]) {
+	//This function
 
-/*
-void genetic() {
+	ac_int<11, false> tempScore;
+	ac_int<7, false> tempIndex;
 
-	ac_int<number_of_nodes, false> chromosome;
-	srand(time(0));
-	for (int i = 0; i < number_of_nodes; i++) {
-		int temp = rand() % 2;
-		//cout << temp;
-		chromosome[i] = temp;
+	for (int i = 0; i < (populationSize - 1); i++) {
+
+		if (vector[i][0] < vector[i + 1][0]) {
+
+			tempScore = vector[i][0];
+			tempIndex = vector[i][1];
+			vector[i][0] = vector[i + 1][0];
+			vector[i][1] = vector[i + 1][1];
+			vector[i + 1][0] = tempScore;
+			vector[i + 1][1] = tempIndex;
+		}
+	}
+}
+
+// ----- FITNESS -----
+void fitness(ac_int<11, false> (&scores)[populationSize], ac_int<11,false> distances[numberOfNodes][numberOfNodes], ac_int<length,false> population[populationSize][numberOfNodes]) {
+	//This function
+	//input distances
+
+	ac_int<length, false> city1;
+	ac_int<length, false> city2;
+
+	//calculate the sum of all the distances for a every chromosome
+	for (int i = 0; i < populationSize; i++) {
+
+		scores[i] = 0;
+
+		for (int j = 0; j < numberOfNodes - 1; j++) {
+
+			city1 = population[i][j];
+			city2 = population[i][j + 1];
+			scores[i] = scores[i] + distances[city1][city2];
+		}
+	}
+	//return a score vector, one score for every chromosome
+	sortByColumn(scores);
+}
+
+// ----- CROSSOVER -----
+void crossover(ac_int<length, false> (&population)[populationSize][numberOfNodes], ac_int<11, false> scores[populationSize]) {
+	// This function
+	// Select the mating pool based on score
+	// top 25% of scores keep it intact]
+	// second 25% of the population is being crossovered
+	// last 2 25% of the population are being randomly regenerated
+
+	short point1;
+	short point2;
+	short size;
+	ac_int<length, false> swapGenes;
+
+	// crossover 25% of the population, 2nd 2th of the population
+	for (int i = (populationSize * 0.25); i < (populationSize * 0.5); i++) {
+
+		point1 = rand() % (numberOfNodes - 10);
+		point2 = rand() % (numberOfNodes - point1) + point1;
+		size = point2 - point1;
+
+		for (int j = point1; j < point2 / 2; ++j) {
+			swapGenes = population[i][j];
+			population[i][j] = population[i][size - j];
+			population[i][size - j] = swapGenes;
+		}
+	}
+	// Regenerate 50% of the population, last 2 4ths of the population
+	for (int i = (populationSize * 0.5); i < populationSize; i++) {
+
+		for (int j = 0; j < numberOfNodes; j++) {
+
+			population[i][j] = j;
+		}
+
+		// Fisher-Yates' shuffling
+		for (int l = 0; l < (numberOfNodes - 1); l++) {
+
+			short k = min(k + 5 + 1, numberOfNodes);
+			ac_int<length, false> temp = population[i][l];
+			population[i][l] = population[i][k];
+		}
+	}
+}
+
+// -----  MUTATION -----
+void mutate(ac_int<length, false>(&population)[populationSize][numberOfNodes]) {
+
+	int index;
+	short point1;
+	short point2;
+	short size;
+	ac_int<length, false> swapGenes;
+
+
+	for (int i = 0; i < 10; i++) {
+
+		index = rand() % populationSize + (populationSize*0.25);
+		point1 = rand() % (numberOfNodes - 10);
+		point2 = rand() % (numberOfNodes - point1) + point1;
+		size = point2 - point1;
+
+		for (int j = point1; j < point2 / 2; ++j) {
+			swapGenes = population[i][j];
+			population[index][j] = population[index][size - j];
+			population[index][size - j] = swapGenes;
+		}
 	}
 
-	int max_generations = 100;
-	int max_epochs = 10;
-	int population = 100;
-	int mutation_rate = 0.2;
+}
 
-	for (int generation = 0; generation < max_generations; generation++) {
-
+// ** NOT THE FINAL MAIN FUNCTION **
+int main() {
+	//create distance matrix for fully connected graph
+	short distance_matrix[numberOfNodes][numberOfNodes];
+	for (int i = 0; i < numberOfNodes; ++i) {
+		for (int j = 0; j < numberOfNodes; ++j) {
+			//distances vary from 40 + 1000 km
+			if (i == j) { continue; }
+			int distance = rand() % 1000 + 40;
+			distance_matrix[i][j] = distance;
+			distance_matrix[j][i] = distance;
+		}
 	}
-}*/
+	return 0;
+}
+
 
